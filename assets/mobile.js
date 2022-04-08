5d853c222242242c35462812de907f109ea09422
liucodes <98996761+liucodes@users.noreply.github.com>
2022-04-08 03:05:22 -0400

    upup

---
+++ assets/mobile.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mobileNav").style.width = "100%";
+    document.getElementById("mobileLinks").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("mobileNav").style.width = "0%";
+}
